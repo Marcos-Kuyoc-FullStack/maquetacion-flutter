9806010c5d6c58337f25e2808718112f90792691
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-11-11 10:58:36 -0600

    feat: se agrega los gradientes y la pagina 2 al PageView

--- lib/screens/scroll_screen.dart
+++ lib/screens/scroll_screen.dart
@@ -3,15 +3,48 @@ import 'package:flutter/material.dart';
 class ScrollScreen extends StatelessWidget {
   const ScrollScreen({super.key});
 
+  final boxDecoration = const BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: [0.5, 0.5],
+      colors: [
+        Color.fromRGBO(127, 237, 202, 1),
+        Color.fromARGB(255, 55, 200, 229),
+      ],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: const [
-          Background(),
-          MainContent(),
-        ],
+      body: Container(
+        decoration: boxDecoration,
+        child: PageView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: const [
+            Page1(),
+            Page2(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        Background(),
+        MainContent(),
+      ],
     );
   }
 }
@@ -65,6 +98,38 @@ class Background extends StatelessWidget {
       alignment: Alignment.topCenter,
       child: const Image(
         image: AssetImage('assets/scroll-1.png'),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 55, 200, 229),
+      child: Center(
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor: const Color.fromRGBO(0, 152, 250, 1),
+            shape: const StadiumBorder(),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              'Bienvenido',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
